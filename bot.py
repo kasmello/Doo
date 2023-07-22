d6d8d2254ab4403f0947867f9aa16166d49bf6e4
kasmello <karmel.gandahusada@gmail.com>
2023-07-22 13:05:29 +0800

    added timer to emoji game, fixed some pairings

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import os
 import json
 import discord
 import random
+import asyncio
 from discord.ext import commands
 
 from dotenv import load_dotenv
@@ -15,6 +16,7 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 intents = discord.Intents.default()
 intents.message_content = True
 
+
 class DooBot(commands.Bot):
     def __init__(self, command_prefix, intents):
         super().__init__(command_prefix, intents=intents)
@@ -22,6 +24,10 @@ class DooBot(commands.Bot):
         self.emoji_dict = None
         self.emoji_score = 0
         self.current_emoji = None
+        self.emoji_time_left = 0
+        with open('phrases.json', 'r', encoding='utf-8') as file:
+
+            self.phrases  = json.load(file)
 
     def reset_emoji_game(self):
         self.playing_emoji_game = False
@@ -30,17 +36,31 @@ class DooBot(commands.Bot):
 
     async def play_emoji_turn(self,ctx):
         self.current_emoji = random.choice(list(self.emoji_dict.keys()))
+        await self.countdown(ctx)
         await ctx.send(f"Current Emoji: {self.current_emoji}")
-
+        
     async def check_emoji(self,ctx,emoji):
+        print(f'{ctx.message.author.name} said {emoji}')
         if emoji in self.emoji_dict[self.current_emoji]:
-            await ctx.send(f"**Good!**")
+            await ctx.send(f"**{random.choice(self.phrases['nice'])}!**")
+            await ctx.send(f"You answered with {self.emoji_time_left} seconds left")
+            if self.emoji_time_left <= 5:
+                await ctx.send(random.choice(self.phrases['hurry up']))
             self.emoji_score += 1
+            self.emoji_time_left = 60
             await self.play_emoji_turn(ctx)
         else:
-            await ctx.send(f"**Ruh Roh!**\nGame over!\n\n\nScore: {self.emoji_score}")
+            await ctx.send(f"**{random.choice(self.phrases['oh no'])}**\nGame over!\n\n\nScore: {self.emoji_score}")
             self.reset_emoji_game()
         
+    async def countdown(self, ctx):
+        while self.emoji_time_left > 0:
+            await asyncio.sleep(1)
+            self.emoji_time_left -= 1
+        await ctx.send(f"**{random.choice(self.phrases['time up'])}**\nGame over!\n\n\nScore: {self.emoji_score}")
+        self.reset_emoji_game()
+        
+
 
 
 bot = DooBot(command_prefix='!',intents=intents)
@@ -65,7 +85,9 @@ async def mcmemojigame(ctx, *args):
 
         bot.emoji_dict  = json.load(file)
         bot.playing_emoji_game = True
-        await ctx.send("**Let's play the emoji game, where anyone can play!🍆🍆🍆**\nI type an emoji, you type one that matches it\nMake sure for each response, you put ***!choose*** beforehand.")
+        await ctx.send("**Let's play the emoji game, where anyone can play!🍆🍆🍆**\nI type an emoji, you type one that matches it. You\
+                       have 60 seconds to decide on an answer before the game is over.\nMake sure for each response, you put ***!choose*** beforehand.")
+        bot.emoji_time_left = 60
         await bot.play_emoji_turn(ctx)
 
 @bot.command()
@@ -73,11 +95,12 @@ async def choose(ctx, *args):
     if bot.playing_emoji_game:
         if len(args) > 1 or len(args[0]) > 1:
             await ctx.send("You sent more than 1 emoji, I'm' only taking the first!")
+        print(args)
         await bot.check_emoji(ctx,args[0][0])
 
 @bot.command()
 async def mcmsad(ctx, *args):
-    await ctx.send('Hold your horses! A sad song is coming!')
+    await ctx.send(f'{random.choice(bot.phrases["wait"])} A sad song is coming!')
     print(f'{ctx.message.author.name} has requested for Joji Lyrics')
     lyrics = return_lyrics()
     for item in lyrics:
